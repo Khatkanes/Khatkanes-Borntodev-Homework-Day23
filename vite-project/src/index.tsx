f7c82648764bfd403ebc7bea2f79a49accf13052
Kutkanes <144354508+Khatkanes@users.noreply.github.com>
2023-11-11 23:24:33 +0700

    Khatkanes
    accept

---
+++ vite-project/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WrappedApp from './components/WrappedApp.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    
+    <WrappedApp></WrappedApp>
+  </React.StrictMode>,
+)
+
